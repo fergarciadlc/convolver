9342c41fa09427111de68a3a2752f579c457d9ae
Fernando Garcia <ferchoget@gmail.com>
2022-09-20 18:07:31 -0500

    initial commit

---
+++ Source/DSP/Convolution.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    Convolution.cpp
+    Created: 20 Sep 2022 6:00:08pm
+    Author:  Fernando Garcia
+
+  ==============================================================================
+*/
+
+#include "Convolution.h"
+
+Convolution::Convolution() {}
+
+Convolution::~Convolution() {}
+
+void Convolution::prepare(double inSampleRate, int inSamplesPerBlock, int inNumChannels)
+{
+    //// load IR file from local wav    
+    //juce::File IRFile = FileHandler.getIRFromIRFolder("Conic Long Echo Hall.wav");
+    //DBG(IRFile.getFullPathName());
+
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = inSampleRate;
+    spec.maximumBlockSize = inSamplesPerBlock;
+    spec.numChannels = inNumChannels;
+
+    // start convolution
+    conv.reset();
+    conv.prepare(spec);
+
+    //// load IR from file
+    //conv.loadImpulseResponse(
+    //    IRFile.getFullPathName(),
+    //    juce::dsp::Convolution::Stereo::yes,
+    //    juce::dsp::Convolution::Trim::yes,
+    //    IRFile.getSize(),
+    //    juce::dsp::Convolution::Normalise::yes
+    //);
+    conv.loadImpulseResponse(
+        BinaryData::conic_long_echo_hall_wav,
+        BinaryData::conic_long_echo_hall_wavSize,
+        juce::dsp::Convolution::Stereo::yes,
+        juce::dsp::Convolution::Trim::yes,
+        0, // request original size of IR
+        juce::dsp::Convolution::Normalise::yes
+      );
+
+}
+
+void Convolution::process(juce::AudioBuffer<float> inBuffer, bool isBypassed)
+{
+    if (isBypassed) { return; }
+    juce::dsp::AudioBlock<float> block(inBuffer);
+    juce::dsp::ProcessContextReplacing<float> context(block);
+    conv.process(context);
+}
---
+++ Source/DSP/Convolution.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    Convolution.h
+    Created: 20 Sep 2022 6:00:08pm
+    Author:  Fernando Garcia
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class Convolution
+{
+public:
+
+    Convolution();
+    ~Convolution();
+
+    void prepare(double inSampleRate, int inSamplesPerBlock, int inNumChannels);
+
+    void process(juce::AudioBuffer<float> inBuffer, bool isBypassed);
+
+private:
+    juce::dsp::Convolution conv;
+    juce::File IRFile;
+
+};
